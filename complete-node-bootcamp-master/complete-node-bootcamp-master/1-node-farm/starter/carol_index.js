6c9832d8194bb977c90b22e952e4589fb269b4f8
Carolina Nogueira <carolina.tinen@alumni.usp.br>
2022-11-05 18:42:48 -0300

    sync and async

--- complete-node-bootcamp-master/complete-node-bootcamp-master/1-node-farm/starter/carol_index.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/1-node-farm/starter/carol_index.js
@@ -1,7 +1,7 @@
 /*const hello = 'Hello World!';
 console.log(hello);
 import fs from 'fs';
-*/
+Blocking, synchronous code
 
 const fs = require('fs');
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,4 +10,7 @@ const textOut = `This is what we know about avocados: ${textIn}!
 Created on ${Date.now()}.`;
 
 fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File successfully written!')
+console.log('File successfully written!');
+*/
+
+//Non-blocking, asynchronous code
